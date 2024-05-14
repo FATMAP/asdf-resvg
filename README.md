2a35a8c1860d291424d131b81545dcfeecdf59b6
Josif Arcimovic <jarcimovic@strava.com>
2024-05-14 13:52:32 +0300

    Turn the clang-format ASDF plugin into the resvg one.

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
-# asdf-clang-format
+# asdf-resvg
 
 
-A `clang-format` plugin for the [asdf version manager](https://asdf-vm.com).
+A `resvg` plugin for the [asdf version manager](https://asdf-vm.com).
 
 
-This plugin will try to install a binary build of clang-format from the official LLVM repository.
-Failing that, it will try to build it from source. Building from source can also be forced by
-setting the `ASDF_CLANG_FORMAT_FORCE_SOURCE_INSTALL=1` environment variable.
+This plugin installs the `resvg` tool from source.
 
 # Contents
 
@@ -17,41 +15,34 @@ setting the `ASDF_CLANG_FORMAT_FORCE_SOURCE_INSTALL=1` environment variable.
 # Dependencies
 
 - `bash`, `curl`, `tar`: generic POSIX utilities.
-- If building from source, a C++ compiler, `cmake`, `make`, etc are required.
+- `cargo`: The Rust package manager.
 
 # Install
 
 Plugin:
 
 ```shell
-asdf plugin add clang-format https://github.com/FATMAP/asdf-clang-format.git
+asdf plugin add resvg https://github.com/FATMAP/asdf-resvg.git
 ```
 
-clang-format:
+resvg:
 
 ```shell
 # Show all installable versions
-asdf list-all clang-format
+asdf list-all resvg
 
 # Install specific version
-asdf install clang-format latest
+asdf install resvg latest
 
 # Set a version globally (on your ~/.tool-versions file)
-asdf global clang-format latest
+asdf global resvg latest
 
-# Now clang-format can be invoked from the shell
-clang-format --version
+# Now resvg can be invoked from the shell
+resvg --version
 ```
 
 Check [asdf](https://github.com/asdf-vm/asdf) readme for more instructions on how to
 install & manage versions.
-
-# Configuration
-
-A following environment variables can affect this plugin:
-
-- `ASDF_CLANG_FORMAT_FORCE_SOURCE_INSTALL`: Set to `1` to force a source-based installation instead of using a
-  pre-compiled binary, even if a binary release is available.
 
 # Acknowledgements
 
--- lib/utils.bash
+++ lib/utils.bash
@@ -2,12 +2,9 @@
 
 set -euo pipefail
 
-ASDF_CLANG_FORMAT_FORCE_SOURCE_INSTALL=${ASDF_CLANG_FORMAT_FORCE_SOURCE_INSTALL:-0}
-
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for <YOUR TOOL>.
-GH_REPO="https://github.com/llvm/llvm-project"
-TOOL_NAME="clang-format"
-TOOL_TEST="clang-format"
+GH_REPO="https://github.com/RazrFalcon/resvg"
+TOOL_NAME="resvg"
+TOOL_TEST="resvg"
 
 fail() {
   echo -e "asdf-$TOOL_NAME: [ERR] $*"
@@ -20,7 +17,6 @@ log() {
 
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if the tool in question is not hosted on GitHub releases.
 if [ -n "${GITHUB_API_TOKEN:-}" ]; then
   curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 fi
@@ -32,8 +28,7 @@ sort_versions() {
 
 list_all_versions() {
   git ls-remote --tags --refs "$GH_REPO" |
-    grep -o -E 'refs/tags/llvmorg-[0-9.]+$' | cut -d/ -f3- |
-    sed 's/^llvmorg-//'
+    grep -o -E 'refs/tags/v[0-9.]+$' | cut -d/ -f3- | sed 's/^v//'
 }
 
 extract() {
@@ -48,7 +43,7 @@ get_source_download_url() {
 
   if [[ "$version" =~ ^[0-9]+\.* ]]; then
     # if version is a release number, form a tag name out of it.
-    echo "https://github.com/llvm/llvm-project/archive/refs/tags/llvmorg-${version}.tar.gz"
+    echo "${GH_REPO}/archive/refs/tags/v${version}.tar.gz"
   else
     # otherwise it can be a branch name or commit sha
     echo "https://github.com/llvm/llvm-project/archive/${version}.tar.gz"
@@ -64,62 +59,6 @@ download_source() {
   url=$(get_source_download_url "$version")
   curl "${curl_opts[@]}" -o "$filepath" "$url" || fail "Could not download $url"
   return 0
-}
-
-download_binary() {
-  local version filepath url
-  version="$1"
-  filepath="$2"
-
-  local platforms=()
-  local kernel arch
-  kernel="$(uname -s)"
-  arch="$(uname -m)"
-
-  case "$kernel" in
-  Darwin)
-    platforms=("${arch}-apple-darwin$(uname -r | cut -d . -f1).0")
-    ;;
-  Linux)
-    platforms=("${arch}-linux-gnu")
-    ;;
-  esac
-
-  log "Downloading $TOOL_NAME release $version..."
-  for platform in ${platforms[*]}; do
-    url="$GH_REPO/releases/download/llvmorg-${version}/clang+llvm-${version}-${platform}.tar.xz"
-    log "Trying ${url} ..."
-    curl "${curl_opts[@]}" -o "$filepath" -C - "$url" && return 0
-  done
-
-  return 1
-}
-
-binary_download_and_extract() {
-  # Puts the extracted files in $ASDF_DOWNLOAD_PATH/bin
-
-  local version=$1
-  local download_dir=$2
-
-  local extract_dir="${download_dir}/bin"
-  mkdir -p "$extract_dir"
-
-  local download_file="${download_dir}/${TOOL_NAME}-${version}-bin.tar.gz"
-
-  if download_binary "$version" "${download_file}"; then
-    extract "${download_file}" "${extract_dir}"
-
-    # Some of the files in the binary archive are write-protected, so when asdf eventually tries
-    # to remove that directory, rm asks a manual confirmation to delete such files. To prevent
-    # that, we make all files in that archive writable.
-    chmod -R u+w "${extract_dir}"
-
-    rm "${download_file}"
-    return 0
-  fi
-
-  rm -rf "${extract_dir}"
-  return 1
 }
 
 source_download_and_extract() {
@@ -154,17 +93,6 @@ download_and_extract() {
     # TODO: support refs
   fi
 
-  if [ "$ASDF_CLANG_FORMAT_FORCE_SOURCE_INSTALL" == 1 ]; then
-    log "Skipping binary download because ASDF_CLANG_FORMAT_FORCE_SOURCE_INSTALL=1"
-  else
-    ## Binary Download & Extract
-    if binary_download_and_extract "$version" "${download_dir}"; then
-      return 0
-    else
-      log "Could not find a suitable binary download for $TOOL_NAME $version, falling back to source..."
-    fi
-  fi
-
   ## Source Download & Extract
   source_download_and_extract "$version" "${download_dir}"
 }
@@ -177,14 +105,10 @@ source_build_install() {
   (
     set -e  # Not sure why, but it doesn't have an effect, so we sprinkle those
             # "|| exit 1" statements.
-    mkdir -p "${build_dir}" || exit 1
-    cd "${build_dir}" || exit 1
-    cmake -DCMAKE_BUILD_TYPE=MinSizeRel "-DCMAKE_INSTALL_PREFIX=${install_path}" \
-      -DBUILD_SHARED_LIBS=OFF "-DLLVM_ENABLE_PROJECTS=clang;clang-tools-extra" \
-      "${src_dir}/llvm" || exit 1
-    make -j "$ASDF_CONCURRENCY" clang-format || exit 1
+    cd "${src_dir}" || exit 1
+    cargo build --release || exit 1
     mkdir -p "${install_path}/bin" || exit 1
-    cp -fp "${build_dir}/bin/clang-format" "${install_path}/bin/" || exit 1
+    cp -fp "${src_dir}/target/release/resvg" "${install_path}/bin/" || exit 1
   )
 }
 
@@ -200,20 +124,8 @@ install_version() {
   (
     mkdir -p "$install_path/bin"
 
-    if [ -d "$ASDF_DOWNLOAD_PATH"/bin ]; then
+    source_build_install "$ASDF_DOWNLOAD_PATH"/src "$ASDF_DOWNLOAD_PATH/build" "$install_path"
 
-      log "Installing download binary in ${ASDF_DOWNLOAD_PATH}/bin"
-      log "Should it fail to work, consider reinstalling with ASDF_CLANG_FORMAT_FORCE_SOURCE_INSTALL=1"
-
-      # Fortunately, all binary releases seem to be built without shared libs,
-      # so copying a single binary turns out to be enough.
-      cp -fp "${ASDF_DOWNLOAD_PATH}/bin/bin/clang-format" "$install_path/bin/"
-
-    elif [ -d "$ASDF_DOWNLOAD_PATH"/src ]; then
-      source_build_install "$ASDF_DOWNLOAD_PATH"/src "$ASDF_DOWNLOAD_PATH/build" "$install_path"
-    fi
-
-    # TODO: Asert <YOUR TOOL> executable exists.
     local tool_cmd
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
     test -x "$install_path/bin/$tool_cmd" || fail "Expected $install_path/bin/$tool_cmd to be executable."
